c38f89af35f7f4fceff44e5d58c186aebc4526cc
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-12-11 11:37:05 +0100

    card claudia

--- pages/p/claudiahernandez.js
+++ pages/p/claudiahernandez.js
@@ -50,7 +50,6 @@ const Card1 = () => {
   return (
     <>
       <Head>
-        <meta charset="UTF-8"></meta>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0"
