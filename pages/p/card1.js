cb7e592b64d93779020fb177ab902fd07ba90f53
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-12-12 14:12:56 +0100

    CSS card1

--- pages/p/card1.js
+++ pages/p/card1.js
@@ -3,6 +3,9 @@ import Head from "next/head";
 import axios from 'axios';
 import IconCaptura from '../../components/icons/captura';
 import IconPhone from '../../components/icons/phone';
+import IconFacebook from '../../components/icons/socials/facebook';
+import IconTwitter from '../../components/icons/socials/twitter';
+import IconGitHub from '../../components/icons/socials/github';
 
 const Card1 = () => {
  
@@ -104,7 +107,7 @@ const Card1 = () => {
                 // onChange={e => agregarPassword(e.target.value) }
               />
 
-              <div className="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"></div>
+              <div className="mx-auto lg:mx-0 w-4/5 pt-3 mb-3 border-b-2 border-green-500 opacity-25"></div>
               <input 
                 className="w-full focus:outline-none focus:ring-4 focus:ring-green-700 focus:ring-opacity-50 focus:border-white rounded-xl py-3 my-2 text-base text-center font-bold flex items-center justify-center lg:justify-start border-2 border-dashed border-green-500"
                 type="text"
@@ -147,7 +150,7 @@ const Card1 = () => {
 
               <div>
                 <div className="mt-1 relative rounded-md">
-                  <div className="absolute inset-y-1 left-1 flex items-center pointer-events-none">
+                  <div className="absolute inset-y-1 flex items-center pointer-events-none">
                     <span className="text-gray-500 sm:text-sm">
                       <IconPhone width={30} height={30}/>
                     </span>
@@ -176,49 +179,22 @@ const Card1 = () => {
 
               <div className="rounded-xl my-8 py-2 px-4 lg:pb-0  lg:w-full mx-auto flex flex-wrap items-center justify-between border-2 border-dashed border-green-500">
                 <a
-                  className="link"
+                  className="h-6 w-6 fill-current text-gray-500 hover:text-green-700"
                   href="#"
-                  data-tippy-content="@facebook_handle"
                 >
-                  <svg
-                    className="h-6 fill-current text-gray-600 hover:text-green-700"
-                    role="img"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <title>Facebook</title>
-                    <path d="M22.676 0H1.324C.593 0 0 .593 0 1.324v21.352C0 23.408.593 24 1.324 24h11.494v-9.294H9.689v-3.621h3.129V8.41c0-3.099 1.894-4.785 4.659-4.785 1.325 0 2.464.097 2.796.141v3.24h-1.921c-1.5 0-1.792.721-1.792 1.771v2.311h3.584l-.465 3.63H16.56V24h6.115c.733 0 1.325-.592 1.325-1.324V1.324C24 .593 23.408 0 22.676 0" />
-                  </svg>
+                  <IconFacebook />
                 </a>
                 <a
-                  className="link"
+                  className="h-6 w-6 fill-current text-gray-500 hover:text-green-700"
                   href="#"
-                  data-tippy-content="@twitter_handle"
                 >
-                  <svg
-                    className="h-6 fill-current text-gray-600 hover:text-green-700"
-                    role="img"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <title>Twitter</title>
-                    <path d="M23.954 4.569c-.885.389-1.83.654-2.825.775 1.014-.611 1.794-1.574 2.163-2.723-.951.555-2.005.959-3.127 1.184-.896-.959-2.173-1.559-3.591-1.559-2.717 0-4.92 2.203-4.92 4.917 0 .39.045.765.127 1.124C7.691 8.094 4.066 6.13 1.64 3.161c-.427.722-.666 1.561-.666 2.475 0 1.71.87 3.213 2.188 4.096-.807-.026-1.566-.248-2.228-.616v.061c0 2.385 1.693 4.374 3.946 4.827-.413.111-.849.171-1.296.171-.314 0-.615-.03-.916-.086.631 1.953 2.445 3.377 4.604 3.417-1.68 1.319-3.809 2.105-6.102 2.105-.39 0-.779-.023-1.17-.067 2.189 1.394 4.768 2.209 7.557 2.209 9.054 0 13.999-7.496 13.999-13.986 0-.209 0-.42-.015-.63.961-.689 1.8-1.56 2.46-2.548l-.047-.02z" />
-                  </svg>
+                  <IconTwitter />
                 </a>
                 <a
-                  className="link"
+                  className="h-6 w-6 fill-current text-gray-500 hover:text-green-700"
                   href="#"
-                  data-tippy-content="@github_handle"
                 >
-                  <svg
-                    className="h-6 fill-current text-gray-600 hover:text-green-700"
-                    role="img"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <title>GitHub</title>
-                    <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
-                  </svg>
+                  <IconGitHub />
                 </a>
                 <a
                   className="link"
